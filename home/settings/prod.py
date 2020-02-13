eec347fa6e672608b09413ae7935f5cd31d20c20
Minh Hung <minhhung19494@gmail.com>
2020-02-13 04:28:38 -0600

    media upload S3

--- home/settings/prod.py
+++ home/settings/prod.py
@@ -42,14 +42,14 @@ AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {    
      'CacheControl': 'max-age=86400',
 }
-DEFAULT_FILE_STORAGE = 'app.storage_backends.MediaStorage'
+AWS_S3_REGION_NAME = 'ap-southeast-1'
+DEFAULT_FILE_STORAGE = 'home.storage_backends.MediaStorage'
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ] 
-STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# MEDIA_URL
+STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, 'static')
 
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 STATICFILES_FINDERS = (           
@@ -58,6 +58,10 @@ STATICFILES_FINDERS = (
 )
 AWS_DEFAULT_ACL = None
 
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+)
 
 # STATIC_URL = '/static/'
 # MEDIA_URL = '/media/'
